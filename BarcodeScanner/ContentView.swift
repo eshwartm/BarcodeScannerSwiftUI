9ddf6539b0da0dececcc490ad9aef8629cbd0786
Eshwar Ramesh <eshwar@metarain.com>
2020-09-04 10:04:52 +0530

    Initial Commit

---
+++ BarcodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Eshwar Ramesh on 9/4/20.
+//  Copyright © 2020 Eshwar Ramesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
